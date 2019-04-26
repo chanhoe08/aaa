866ed88acb344bfb8efb14a294d9c77a774656ad
chanhoe08 <50009247+chanhoe08@users.noreply.github.com>
2019-04-26 16:45:45 +0900

    Create 7장 문자열.py

---
+++ 7장 문자열.py
@@ -0,0 +1,35 @@
+# year=str(input("생년월일을 입력하세요:"))
+# if year[4:6]>str(12):
+#     print("올바른 생년월일이 아닙니다.")
+# elif year[6:8]>str(31):
+#     print("올바른 생년월일이 아닙니다.")
+# else:
+#     print("올바른 생년월일입니다.")
+
+# s=str(input("문자열을 입력하세요:"))
+# rs="".join(reversed(s))
+# print(rs)
+
+# s=str(input("문자열을 입력하세요:"))
+# rs=''
+# for i in range(len(s)-1,-1,-1):
+#     rs+=s[i]
+# print(rs)
+
+# s=str(input("문자열을 입력하세요:"))
+# rs=''
+# for i in range(len(s)-1,-1,-1):
+#     rs+=s[i]
+# if rs==s:
+#     print("회문입니다.")
+# else:
+#     print("회문이 아닙니다.")
+
+# s=str(input("문자열을 입력하세요:"))
+# rs="".join(reversed(s))
+# if rs==s:
+#     print("회문입니다.")
+# else:
+#     print("회문이 아닙니다.")
+
+
